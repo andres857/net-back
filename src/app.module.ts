8c9fe4b6ec283e7ed00ea0e051c20252c5d613da
andres <andres857@gmaill.com>
2025-05-06 15:59:19 -0500

    se dockeriza el project para desarrollo

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,11 +14,11 @@ import { RssFeed } from './rss/entities/rss-feed.entity';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DATABASE_HOST || 'localhost',
-      // port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-      port: 5411,
+      port: parseInt(process.env.DATABASE_PORT ?? '5432', 10),
+      // port: 5432,
+      database: process.env.DATABASE_NAME || 'net_db',
       username: process.env.DATABASE_USER || 'postgres',
       password: process.env.DATABASE_PASSWORD || 'postgres',
-      database: process.env.DATABASE_NAME || 'postgres',
       entities: [RssFeed],
       synchronize: true, // Solo para desarrollo
     }),
